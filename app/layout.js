d1fcddd921807302daef302adc40f8a4bc537f14
Alex Guerra <alexander.a.guerra@gmail.com>
2023-12-11 12:05:41 -0600

    Add @vercel/speed-insights dependency and import SpeedInsights component

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>{children}</body>
+            <SpeedInsights />
         </html>
     )
 }
