d479c0d1cf56d7e038beb7f14825d80ede5787f9
Alex Guerra <alexander.a.guerra@gmail.com>
2023-12-11 12:32:16 -0600

    Update import paths and fix class names in Header component

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,5 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import Head from 'next/head'
 
 export default function About() {
     return (
--- components/Header.js
+++ components/Header.js
@@ -2,20 +2,20 @@ export default function Header() {
     return (
         <header className="bg-gray-900 text-gray-100">
             <nav className="container mx-auto flex items-center justify-between py-4">
-                <a href="#" class="text-xl font-bold">
+                <a href="#" className="text-xl font-bold">
                     Logo
                 </a>
                 <div>
-                    <a href="#" class="mr-4">
+                    <a href="#" className="mr-4">
                         Link 1
                     </a>
-                    <a href="#" class="mr-4">
+                    <a href="#" className="mr-4">
                         Link 2
                     </a>
-                    <a href="#" class="mr-4">
+                    <a href="#" className="mr-4">
                         Link 3
                     </a>
-                    <a href="#" class="mr-4">
+                    <a href="#" className="mr-4">
                         Link 4
                     </a>
                 </div>
