ac3c24df3d2d1bd19f407e45184b371340c70429
Alex Guerra <alexander.a.guerra@gmail.com>
2023-12-12 02:05:23 -0600

    Update Sanity configuration files and add gear schema

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,7 +2,8 @@ import { blockContent } from './schemas/blockContent'
 import { category } from './schemas/category'
 import { post } from './schemas/post'
 import { author } from './schemas/author'
+import { gear } from './schemas/gear'
 
 export const schema = {
-    types: [post, author, category, blockContent],
+    types: [post, author, category, blockContent, gear],
 }
---
+++ sanity/schemas/gear.js
@@ -0,0 +1,27 @@
+export const gear = {
+    name: 'gear',
+    title: 'Gear',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+        },
+        {
+            name: 'link',
+            title: 'Link',
+            type: 'url',
+        },
+    ],
+}
