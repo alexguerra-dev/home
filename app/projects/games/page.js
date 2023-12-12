21691b623bc2f6d5b707a33ea7079af495dd7e9e
Alex Guerra <alexander.a.guerra@gmail.com>
2023-12-11 21:24:03 -0600

    Fix import statements for Footer component

--- app/projects/games/page.js
+++ app/projects/games/page.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import Header from '@/components/Header'
-import Footer from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default function Games() {
     return (
@@ -18,24 +18,24 @@ export default function Games() {
                         width={500}
                         height={500}
                     />
-                    <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">
                             The Coldest Sunset
                         </div>
-                        <p class="text-gray-700 text-base">
+                        <p className="text-gray-700 text-base">
                             Lorem ipsum dolor sit amet, consectetur adipisicing
                             elit. Voluptatibus quia, nulla! Maiores et
                             perferendis eaque, exercitationem praesentium nihil.
                         </p>
                     </div>
-                    <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                             #photography
                         </span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                             #travel
                         </span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                             #winter
                         </span>
                     </div>
